eb04c72f0ab0694020312703b4c148e56bc50568
jssatorres <juansataz.dev@gmail.com>
2023-06-24 11:24:01 +0200

    feat (read_html)

---
+++ tables_win.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+all_tables = pd.read_html('https://web.archive.org/web/20221115040351/https://en.wikipedia.org/wiki/2022_FIFA_World_Cup')
+
+# print(all_tables[10])
+# print('\n') 
+# print(all_tables[11])
+# print('\n') 
+print(all_tables[12])
+print('\n') 
+print(all_tables[19])
+print('\n') 
+print(all_tables[26])
+print('\n') 
