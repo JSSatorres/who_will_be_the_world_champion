7da95b6834b92fe32d3cc9a66681675361ad1d19
jssatorres <juansataz.dev@gmail.com>
2023-06-30 08:31:01 +0200

    feat: add todo

--- eurolige_games2022-2023.py
+++ eurolige_games2022-2023.py
@@ -1,5 +1,7 @@
 from bs4  import BeautifulSoup
 import requests
+
+# TODO: imposible to read div
 web = 'https://www.sofascore.com/real-madrid-olympiacos-bc/bvbsPvb#11274336'
 
 response = requests.get(web)
