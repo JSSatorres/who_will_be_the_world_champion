78cac5b783bd06d03c67d87418db407c1faeac58
jssatorres <juansataz.dev@gmail.com>
2023-06-30 08:27:50 +0200

    feat (beatifulSoup) wip

--- wf.py
+++ wf.py
@@ -2,12 +2,18 @@ from year_data import year
 from bs4  import BeautifulSoup
 import requests
 
-web = 'https://en.wikipedia.org/wiki/2014_FIFA_World_Cup'
+web = 'https://es.wikipedia.org/wiki/Copa_Mundial_de_F%C3%BAtbol_de_2014'
 
 response = requests.get(web)
 
 content = response.text
-BeautifulSoup(content, 'lxml')
+soup =BeautifulSoup(content, 'lxml')
 
+# matches = soup.find_all('table', class_='collapsible autocollapse vevent plainlist')
+# for match in matches:
+#     print(match)
 
-
+matches = soup.select("tbody > tr > td ")
+for match in matches:
+    print(match)
+#collapsibleTable0 > tbody > tr:nth-child(1)
