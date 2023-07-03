e25b2b859d694aeee97b9fcb2acb2ab01b412a9e
jssatorres <juansataz.dev@gmail.com>
2023-07-03 18:53:16 +0200

    feat: extract matches wip

--- wf.py
+++ wf.py
@@ -9,11 +9,31 @@ response = requests.get(web)
 content = response.text
 soup =BeautifulSoup(content, 'lxml')
 
-# matches = soup.find_all('table', class_='collapsible autocollapse vevent plainlist')
+
+
+matches = soup.find_all('table', class_='collapsible autocollapse vevent plainlist')
+
+games = []
+
+for match in matches:
+    # match.select('tr:nth-child(1) > td:nth-child(2) > a')
+    # print(match)
+    team_links = match.select('tr:nth-child(1) > td:nth-child(2) > a[title^="Selección de fútbol de"]')
+    team_names = [link.text for link in team_links]
+    game = '-'.join(team_names)
+    games.append(game)
+    # games += [(game)]
+    
+    # score_cells = match.select('td[align="right"] > div > b')
+    # scores = [cell.text for cell in score_cells]
+    
+print(games)
+
+# print (soup)
+# matches = soup.select("#collapsibleTable0 > tbody > tr:nth-child(1)")
 # for match in matches:
 #     print(match)
-
-matches = soup.select("tbody > tr > td ")
-for match in matches:
-    print(match)
 #collapsibleTable0 > tbody > tr:nth-child(1)
+
+
+#collapsibleTable6 > tbody > tr:nth-child(1) > td:nth-child(2) > a
