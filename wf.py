20fddc6e6b055dd1256bece3ff6aa794b8ace677
jssatorres <juansataz.dev@gmail.com>
2023-07-03 19:20:21 +0200

    feat: join matches without score

--- wf.py
+++ wf.py
@@ -15,24 +15,37 @@ matches = soup.find_all('table', class_='collapsible autocollapse vevent plainli
 
 games = []
 
-for match in matches:
-    # match.select('tr:nth-child(1) > td:nth-child(2) > a')
-    # print(match)
-    team_links = match.select('tr:nth-child(1) > td:nth-child(2) > a[title^="Selección de fútbol de"]')
-    team_names = [link.text for link in team_links]
-    print(team_names)
-    game = '-'.join(team_names)
-    print(game)
-    print('------------------')
+# for match in matches:
+#     # match.select('tr:nth-child(1) > td:nth-child(2) > a')
+#     # print(match)
+#     team_links = match.select('tr:nth-child(1) > td:nth-child(2) > a[title^="Selección de fútbol de"]')
+#     team_names = [link.text for link in team_links]
+#     print(team_names)
+#     game = '-'.join(team_names)
+#     print(game)
+#     print('------------------')
+#     games.append(game)
+#     print(games)
+
+#     print('-------XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-----------')
+for i in range(0, len(matches), 2):
+    match1 = matches[i]
+    match2 = matches[i+1]
+
+    team_links1 = match1.select('tr:nth-child(1) > td:nth-child(2) > a[title^="Selección de fútbol de"]')
+    team_names1 = [link.text for link in team_links1]
+
+    team_links2 = match2.select('tr:nth-child(1) > td:nth-child(2) > a[title^="Selección de fútbol de"]')
+    team_names2 = [link.text for link in team_links2]
+
+    game = f"{team_names1[0]}-{team_names2[0]}"
     games.append(game)
-    print(games)
-    print('-------XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-----------')
     # games += [(game)]
-    
+print(games)   
     # score_cells = match.select('td[align="right"] > div > b')
     # scores = [cell.text for cell in score_cells]
     
-print(games)
+# print(games)
 
 # print (soup)
 # matches = soup.select("#collapsibleTable0 > tbody > tr:nth-child(1)")
