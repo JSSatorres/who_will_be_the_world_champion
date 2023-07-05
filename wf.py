d56c30a5ab588fe207d4959aece58bf392b8b694
jssatorres <juansataz.dev@gmail.com>
2023-07-05 17:30:25 +0200

    feat: df all matches

--- wf.py
+++ wf.py
@@ -1,31 +1,37 @@
-from year_data import year
+from year_data import years
 from bs4  import BeautifulSoup
 import requests
+import pandas as pd
 
-web = 'https://es.wikipedia.org/wiki/Copa_Mundial_de_F%C3%BAtbol_de_2014'
+def get_all_matches(year):    
+    web = f'https://es.wikipedia.org/wiki/Copa_Mundial_de_F%C3%BAtbol_de_{year}'
 
-response = requests.get(web)
+    response = requests.get(web)
 
-content = response.text
-soup =BeautifulSoup(content, 'lxml')
+    content = response.text
+    soup =BeautifulSoup(content, 'lxml')
 
+    matches = soup.find_all('table', class_='collapsible autocollapse vevent plainlist')
 
+    home = []
+    score = []
+    away = []
 
-matches = soup.find_all('table', class_='collapsible autocollapse vevent plainlist')
+    for match in matches:
+        local_team = match.select('tr:nth-child(1) > td:nth-child(2) > a[title^="Selección de fútbol de"]')
+        home.append(local_team[0].text if local_team else "")
+        
+        scoreMatch = match.select('tr:nth-child(1) > td:nth-child(3) > div > b')
+        score.append(scoreMatch[0].text if score else "")
+        
+        visitor_team = match.select('tr:nth-child(1) > td:nth-child(4) > a[title^="Selección de fútbol de"]')
+        away.append(visitor_team[0].text if visitor_team else "")
 
-games = []
+    dict_football= {'home':home,'score':score,'away':away}
+    df_matches = pd.DataFrame(dict_football)
+    df_matches['year']= year  
+    return df_matches 
 
-for match in matches:
-    local_team = match.select('tr:nth-child(1) > td:nth-child(2) > a[title^="Selección de fútbol de"]')
-    local_team_text = local_team[0].text if local_team else ""
-    
-    score = match.select('tr:nth-child(1) > td:nth-child(3) > div > b')
-    score_text = score[0].text if score else ""
-    
-    visitor_team = match.select('tr:nth-child(1) > td:nth-child(4) > a[title^="Selección de fútbol de"]')
-    visitor_team_text = visitor_team[0].text if visitor_team else ""
-    
-    game = f"{local_team_text}-{visitor_team_text}-{score_text}"
-    games.append(game)
-    # games += [(game)]
-print(games)   
+fifa = [get_all_matches(year) for year in years ] 
+df_fifa = pd.concat(fifa, ignore_index=True)
+df_fifa.to_csv('fifa_worldcup_historical_data.csv', index=False)
