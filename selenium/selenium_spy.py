7042c40a4e815108cf3c43e6719ba167aa000c9c
jssatorres <juansataz.dev@gmail.com>
2023-07-19 07:40:14 +0200

    fix: change the web target

--- selenium/selenium_spy.py
+++ selenium/selenium_spy.py
@@ -14,19 +14,20 @@ driver = webdriver.Chrome(service=service)
 # Abrir la página web
 
 # web1='https://www.sofascore.com/tournament/basketball/international/euroleague/138#42914'
-web = 'https://www.olx.pt/bebes-criancas/relaxar-e-dormir/'
+web = 'https://as.com//'
+# web = 'https://www.olx.pt/bebes-criancas/relaxar-e-dormir/'
 driver.get(web)
 
 # todos los elementos
-carricoches_bebe = driver.find_elements(by='xpath',value='//*[@id="647203475"]')
-print(carricoches_bebe)
+article = driver.find_elements(by='xpath',value="//article[contains(@class, 's s--h')]")
+print(article)
 
-for coche in carricoches_bebe:    
-  title = coche.find_element_by_xpath('.//*[@id="515250869"]/a/div/div/div[2]/div[1]/h6')
-  # title = coche.find_element_by_xpath('.//*[@id="515250869"]/a/div/div/div[2]/div[1]/h6')
-  print(title)  
+# for coche in article:    
+#   title = coche.find_elements(by='xpath',value='///a/div/div/div[2]/div[1]/h6')
+#   # title = coche.find_element_by_xpath('.//*[@id="515250869"]/a/div/div/div[2]/div[1]/h6')
+#   print(title)  
 
-input("Presiona una tecla para cerrar el navegador...")
+# input("Presiona una tecla para cerrar el navegador...")
 
 # Cerrar el navegador
-driver.quit()
+# driver.quit()
