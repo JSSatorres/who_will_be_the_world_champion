1adeec1edd7e814d58d667bed108b1afa7910cb1
jssatorres <juansataz.dev@gmail.com>
2023-07-21 18:20:11 +0200

    feat: sscraping titles airnbnb

--- selenium/airbnb.py
+++ selenium/airbnb.py
@@ -9,34 +9,15 @@ from selenium.webdriver.chrome.service import Service
 opts = Options()
 opts.add_argument("user-agent= Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
 
-webdriver.Chrome(
-	service=Service('./chromedriver'),
+driver = webdriver.Chrome( 
+	service=Service('./chromedriver.exe'),
 	options=opts
 )
 
-# driver_path = 'D:\programacion\python\who_will_be_champion\selenium' 
-# service = Service(executable_path=driver_path)
+web = 'https://www.airbnb.es/'
+driver.get(web)
+sleep(3)
 
-# driver = webdriver.Chrome(service=service)
-# # driver = webdriver.Chrome(ChromeDriverManager().install())
-
-# # Abrir la página web
-
-# # web1='https://www.sofascore.com/tournament/basketball/international/euroleague/138#42914'
-# web = 'https://as.com//'
-# # web = 'https://www.olx.pt/bebes-criancas/relaxar-e-dormir/'
-# driver.get(web)
-
-# # todos los elementos
-# articles = driver.find_elements(by='xpath',value=".//article[contains(@class, 's s--h')]")
-# # print(article)
-# # wait = WebDriverWait(driver, 10) 
-# for article in articles:    
-#   title = article.find_element(by='xpath',value=".//a[contains(@class, 's__tl')]")
-#   # title = coche.find_element_by_xpath('.//*[@id="515250869"]/a/div/div/div[2]/div[1]/h6')
-#   print(title)  
-
-input("Presiona una tecla para cerrar el navegador...")
-
-# Cerrar el navegador
-# driver.quit()
+titles = driver.find_elements(By.XPATH,'//div[@data-testid="listing-card-title"]')
+for title in titles:
+	print(title.text)
